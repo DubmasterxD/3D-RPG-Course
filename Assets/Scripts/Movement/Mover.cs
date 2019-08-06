c0fa156dee88c48e138491cc9011471993c86e50
DubmasterxD <dubmaster@tlen.pl>
2019-08-06 23:32:32 +0200

    Basic movement finished with following camera and animations

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        float forwardSpeed = Vector3.Distance(Vector3.zero, navMeshAgent.velocity);
+        anim.SetFloat("forwardSpeed", forwardSpeed);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            navMeshAgent.destination = hit.point;
+        }
+    }
+}
