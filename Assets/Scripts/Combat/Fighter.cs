1fa1179da0fe5cf524015766194ad3adfa937671
DubmasterxD <dubmaster@tlen.pl>
2019-08-08 05:22:10 +0200

    Targetting, moving towards an enemy and stopping movement in specific range. Untargetting enemy.

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+        Mover mover;
+
+        private void Start()
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            if(target!=null)
+            {
+                if (Vector3.Distance(transform.position, target.position) > weaponRange)
+                {
+                    mover.MoveTo(target.position);
+                }
+                else
+                {
+                    mover.StopMoving();
+                }
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,45 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField] Transform target;
-    NavMeshAgent navMeshAgent;
-    Animator anim;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Mover : MonoBehaviour
     {
-        anim = GetComponent<Animator>();
-        navMeshAgent = GetComponent<NavMeshAgent>();
-    }
+        NavMeshAgent navMeshAgent;
+        Animator anim;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
+        void Start()
         {
-            MoveToCursor();
+            anim = GetComponent<Animator>();
+            navMeshAgent = GetComponent<NavMeshAgent>();
         }
-        UpdateAnimator();
-    }
 
-    private void UpdateAnimator()
-    {
-        float forwardSpeed = Vector3.Distance(Vector3.zero, navMeshAgent.velocity);
-        anim.SetFloat("forwardSpeed", forwardSpeed);
-    }
-
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        void Update()
         {
-            navMeshAgent.destination = hit.point;
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            float forwardSpeed = Vector3.Distance(Vector3.zero, navMeshAgent.velocity);
+            anim.SetFloat("forwardSpeed", forwardSpeed);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            if (navMeshAgent.isStopped)
+                navMeshAgent.isStopped = false;
+        }
+
+        public void StopMoving()
+        {
+            if (!navMeshAgent.isStopped)
+                navMeshAgent.isStopped = true;
         }
     }
 }
